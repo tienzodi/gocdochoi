f245253b33a0f39f3eb1f42eadfdb8d4d48decb9
Tien <tien@zodinet.com>
2016-10-09 20:30:20 +0700

    update layout + database

--- admin/config.php
+++ admin/config.php
@@ -1,25 +1,26 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://gocdochoi.local:88/admin/');
-define('HTTP_CATALOG', 'http://gocdochoi.local:88/');
+define('HTTP_SERVER', 'http://gocdochoi.com/admin/');
+define('HTTP_CATALOG', 'http://gocdochoi.com/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://gocdochoi.local:88/admin/');
-define('HTTPS_CATALOG', 'http://gocdochoi.local:88/');
+define('HTTPS_SERVER', 'http://gocdochoi.com/admin/');
+define('HTTPS_CATALOG', 'http://gocdochoi.com/');
 
 // DIR
-define('DIR_APPLICATION', 'E:/gocdochoi.com/admin/');
-define('DIR_SYSTEM', 'E:/gocdochoi.com/system/');
-define('DIR_IMAGE', 'E:/gocdochoi.com/image/');
-define('DIR_LANGUAGE', 'E:/gocdochoi.com/admin/language/');
-define('DIR_TEMPLATE', 'E:/gocdochoi.com/admin/view/template/');
-define('DIR_CONFIG', 'E:/gocdochoi.com/system/config/');
-define('DIR_CACHE', 'E:/gocdochoi.com/system/storage/cache/');
-define('DIR_DOWNLOAD', 'E:/gocdochoi.com/system/storage/download/');
-define('DIR_LOGS', 'E:/gocdochoi.com/system/storage/logs/');
-define('DIR_MODIFICATION', 'E:/gocdochoi.com/system/storage/modification/');
-define('DIR_UPLOAD', 'E:/gocdochoi.com/system/storage/upload/');
-define('DIR_CATALOG', 'E:/gocdochoi.com/catalog/');
+define('BASE_DIR', str_replace(DIRECTORY_SEPARATOR.'admin', '', realpath(dirname(__FILE__))));
+define('DIR_APPLICATION', BASE_DIR.'/admin/');
+define('DIR_SYSTEM', BASE_DIR.'/system/');
+define('DIR_IMAGE', BASE_DIR.'/image/');
+define('DIR_LANGUAGE', BASE_DIR.'/admin/language/');
+define('DIR_TEMPLATE', BASE_DIR.'/admin/view/template/');
+define('DIR_CONFIG', BASE_DIR.'/system/config/');
+define('DIR_CACHE', BASE_DIR.'/system/storage/cache/');
+define('DIR_DOWNLOAD', BASE_DIR.'/system/storage/download/');
+define('DIR_LOGS', BASE_DIR.'/system/storage/logs/');
+define('DIR_MODIFICATION', BASE_DIR.'/system/storage/modification/');
+define('DIR_UPLOAD', BASE_DIR.'/system/storage/upload/');
+define('DIR_CATALOG', BASE_DIR.'/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
--- config.php
+++ config.php
@@ -1,22 +1,23 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://gocdochoi.local:88/');
+define('HTTP_SERVER', 'http://gocdochoi.com/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://gocdochoi.local:88/');
+define('HTTPS_SERVER', 'http://gocdochoi.com/');
 
 // DIR
-define('DIR_APPLICATION', 'E:/gocdochoi.com/catalog/');
-define('DIR_SYSTEM', 'E:/gocdochoi.com/system/');
-define('DIR_IMAGE', 'E:/gocdochoi.com/image/');
-define('DIR_LANGUAGE', 'E:/gocdochoi.com/catalog/language/');
-define('DIR_TEMPLATE', 'E:/gocdochoi.com/catalog/view/theme/');
-define('DIR_CONFIG', 'E:/gocdochoi.com/system/config/');
-define('DIR_CACHE', 'E:/gocdochoi.com/system/storage/cache/');
-define('DIR_DOWNLOAD', 'E:/gocdochoi.com/system/storage/download/');
-define('DIR_LOGS', 'E:/gocdochoi.com/system/storage/logs/');
-define('DIR_MODIFICATION', 'E:/gocdochoi.com/system/storage/modification/');
-define('DIR_UPLOAD', 'E:/gocdochoi.com/system/storage/upload/');
+define('BASE_DIR', realpath(dirname(__FILE__)));
+define('DIR_APPLICATION', BASE_DIR.'/catalog/');
+define('DIR_SYSTEM', BASE_DIR.'/system/');
+define('DIR_IMAGE', BASE_DIR.'/image/');
+define('DIR_LANGUAGE', BASE_DIR.'/catalog/language/');
+define('DIR_TEMPLATE', BASE_DIR.'/catalog/view/theme/');
+define('DIR_CONFIG', BASE_DIR.'/system/config/');
+define('DIR_CACHE', BASE_DIR.'/system/storage/cache/');
+define('DIR_DOWNLOAD', BASE_DIR.'/system/storage/download/');
+define('DIR_LOGS', BASE_DIR.'/system/storage/logs/');
+define('DIR_MODIFICATION', BASE_DIR.'/system/storage/modification/');
+define('DIR_UPLOAD', BASE_DIR.'/system/storage/upload/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
